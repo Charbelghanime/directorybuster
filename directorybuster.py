625264307e916bf3f01427124187f0002e69cd6a
Charbelghanime <80881671+Charbelghanime@users.noreply.github.com>
2024-04-13 10:58:25 +0300

    Update directorybuster.py

--- directorybuster.py
+++ directorybuster.py
@@ -3,8 +3,7 @@
 This tool is designed for security testing purposes to identify accessible directories or paths on a web server by attempting to access them with common directory names. 
 It performs HTTP GET requests to the specified target website and checks the responses against a user-defined list of valid HTTP status codes.
 
-Usage: python3 directorybuster.py [-h] -w WORDLIST [-c STATUS_CODES] [-t TIMEOUT]
-                          base_url
+Usage: python3 directorybuster.py [-h] -w [WORDLIST] [-c STATUS_CODES] [-t TIMEOUT] base_url
 Created by: Charbel Ghanime
 
 '''
